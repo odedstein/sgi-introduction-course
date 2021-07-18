90b5c6ffc3a86299b64671cfb70ab8298b507695
sgsellan <sgsellan@gmail.com>
2021-07-18 18:10:00 -0400

    empty files

---
+++ 201_polylines/exercise/catmull_rom_interpolation.m
@@ -0,0 +1,13 @@
+function Pt = catmull_rom_interpolation(P,t)
+% P is a set of points, which we assume are corresponded to equally spaced
+% times between 0 and 1. t is a scalar value between 0 and 1. Return the
+% value of the catmull-rom interpolation of P at time t. You may use
+% cubic_hermite.m, 
+
+
+P_times = linspace(0,1,size(P,1));
+
+
+
+
+end
