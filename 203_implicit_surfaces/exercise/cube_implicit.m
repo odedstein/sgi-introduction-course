b39582dc6110dfcffcfeaafb207d43741e46089e
Silvia Sellán <sgsellan@gmail.com>
2024-07-04 12:42:35 -0400

    mesh exercises

--- 203_implicit_surfaces/exercise/cube_implicit.m
+++
@@ -1,6 +0,0 @@
-function I = cube_implicit(side,center,P)
-% Calculate the value of an implicit function representing a cube of a
-% given side and center at a given *set* of points P
-
-
-end
--- 203_implicit_surfaces/exercise/make_inigo_proud.m
+++
@@ -1,6 +0,0 @@
-function I = make_inigo_proud(P,t)
-% Calculate the value of an implicit function of your choosing
-% at a given *set* of points P (and at a time t if you want). Be creative!
-
-
-end
