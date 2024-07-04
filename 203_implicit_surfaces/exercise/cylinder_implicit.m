4bc244055fcb4987b122c3a6362146f5255cd79b
Silvia Sellán <sgsellan@gmail.com>
2024-07-04 11:29:54 -0400

    mesh exercises

---
+++ 203_implicit_surfaces/exercise/cylinder_implicit.m
@@ -0,0 +1,6 @@
+function I = cylinder_implicit(center,radius,height,P)
+% Calculate the value of an implicit function representing a cylinder of a
+% given radius, height and center at a given *set* of points P
+
+
+end
